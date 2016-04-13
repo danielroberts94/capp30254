7e73796d2cb94782a00040ea8539ce8a7ea4b3e8
dtroberts94 <danielroberts@uchicago.edu>
2016-04-12 23:28:45 -0500

    assignment 2 submission

--- assignment2/pipeline.py
+++ assignment2/pipeline.py
@@ -1,3 +1,5 @@
+# By Daniel Roberts
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,10 +8,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 
+# Function to read in csv data
 def data_read(file_name):
     data = pd.read_csv(file_name)
     return data
 
+#Function to explore data by printing out summary statistics and producing histograms for numerical
+#data and bar charts for categorical data
 def data_explore(data, dataset_name):
     stats = data.describe()
     print stats
@@ -43,7 +48,8 @@ def data_explore(data, dataset_name):
         except:
             plt.close()
             continue
-def mean_median_fill(data):
+#Simple function to fill variable missing vals with mean or mode of the variables
+def mean_mode_fill(data):
     num_data = data.select_dtypes(include = ['number'])
     str_data = data.select_dtypes(include = ['object'])
     if len(num_data.columns) > 0:
@@ -53,15 +59,19 @@ def mean_median_fill(data):
     data.to_csv("data_output/fill_output.csv")
     return data
 
+#function to discretize a numerical variable into a set number of quantiles.
 def data_discretize(data, col_names, quantiles):
     for col, quant in zip(col_names,quantiles):
         data.update(pd.qcut(data[col], q = quant))
     return data
 
+#function to create a series of binary variables from a categorical variable
+#aka "Dummy Variables"
 def data_dummify(data, col_names, quantiles):
     data = pd.get_dummies(data, columns = col_names)
     return data
 
+#Builds one of three types of classifiers at present. Future versions will be amenable to parameter choice
 def build_classifier(train_data, learnvar, feat_names, clf_name):
     train_feats = train_data[feat_names]
     train_learnvar = train_data[learnvar]
@@ -75,11 +85,13 @@ def build_classifier(train_data, learnvar, feat_names, clf_name):
     clf = clfs[clf_name]()
     clf.fit(train_feats,train_learnvar)
     return clf
-
+#Predict values based on a given classifer (note: not classifier name)
 def clf_predict(clf, test_data, feat_names):
     test_feats = test_data[feat_names]
+    
     pred_values = clf.predict(test_feats)
     return pred_values
 
+#Determine accuracy from a given classifier
 def clf_accuracy(clf, predictors, predicted):
     return clf.score(predictors, predicted)
