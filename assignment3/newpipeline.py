91bff5330eaacff2f0609d328658bfc5e04a7538
dtroberts94 <danielroberts@uchicago.edu>
2016-05-06 09:37:10 -0500

    temp

--- assignment3/newpipeline.py
+++ assignment3/newpipeline.py
@@ -1,12 +1,32 @@
 # By Daniel Roberts
 
-import pandas as pd
-import numpy as np
+
+import numpy as np 
+import pandas as pd 
+from scipy import optimize
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
-import requests
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import LinearSVC
+from patsy import dmatrices
+import multiprocessing as mp
+from multiprocessing import Process, Queue
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from sklearn.cross_validation import cross_val_score
+import os, timeit, sys, itertools, re, time, requests, random, functools, logging, csv, datetime
+import seaborn as sns
+from sklearn import linear_model, neighbors, ensemble, svm, preprocessing
+from numba.decorators import jit, autojit
+from numba import double #(nopython = True, cache = True, nogil = True [to run concurrently])
+from sklearn import preprocessing, cross_validation, svm, metrics, tree, decomposition, svm
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier, OrthogonalMatchingPursuit, RandomizedLogisticRegression
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.grid_search import ParameterGrid
+from sklearn.metrics import *
+from sklearn.preprocessing import StandardScaler
+from time import time 
 
 ##################################################################################
 '''
@@ -110,9 +130,30 @@ def data_dummify(data, col_names, quantiles):
     return data
 
 #Builds one of three types of classifiers at present. Future versions will be amenable to parameter choice
-def build_classifier(train_data, learnvar, feat_names, clf_name):
-    train_feats = train_data[feat_names]
-    train_learnvar = train_data[learnvar]
+def build_classifier(X_var, y_var, model_dict):
+    params = makeDicts(model_dict)
+    model_num = len(params)
+    results = [None]*total
+    folds = cross_validation.KFold(len(y_var), k)
+
+    z = 0
+    for pars in params:
+        clf = fun_build(model_dict['model'], pars)
+        i = 0
+        for train, test in kf:
+            x_train, x_test = X._slice(train, 0), X._slice(test, 0)
+            y_train, y_test = y._slice(train, 0), y._slice(test, 0)
+            yTests[i] - yTest
+
+            train_start = time()
+            model_fit = clf.fit(x_train,y_train)
+            train_time = time() - train_start
+            train_times[i] = train_time
+            test_start = time()
+
+
+
+            
     clfs = {'log': LogisticRegression,
             'Kneighbors': KNeighborsClassifier,
             'LSVC': LinearSVC}
@@ -124,6 +165,59 @@ def build_classifier(train_data, learnvar, feat_names, clf_name):
     clf.fit(train_feats,train_learnvar)
     return clf
 #Predict values based on a given classifer (note: not classifier name)
+def evaluation(y_test, predProbs, train_times, test_times, accuracies, model_name):
+    prec_giv_threshhold = ['Precision at .05', 'Precision at .10', 'Precision at .2', 'Precision at .25', 'Precision at .5', 'Precision at .75', 'Precision at .85']
+    recall_giv_threshhold = ['Recall at .05', 'Recall at .10', 'Recall at .20', 'Recall at .25', 'Recall at .5', 'Recall at .75', 'Recall at .85']
+    levels= [.05, .1, .2, .25, .5, .75, .85]
+    levnums = len(levels)
+    results = {}
+    num_folds = len(y_test)
+    folds = range(0, num_folds)
+    results['Model'] = model_name
+    for x in xrange(0, tots):
+        threshhold = amts[x]
+        prec = [precision_at_k(y_test[j], predProbs[j], threshhold)  for j in folds]
+    auc = [metrics.roc_auc_score(yTests[j], predProbs[j]) for j in folds]
+    aucStd = np.std(auc)
+    aucM = np.mean(auc)
+    trainM = np.mean(train_times)
+    trainStd = np.std(train_times)
+    testM = np.mean(test_times)
+    testStd = np.std(test_times)
+    accM = np.mean(accuracies)
+    accStd = np.std(accuracies)
+
+    res['AUC'] = makeResultString(aucM, aucStd)
+    res['train_time (sec)'] = makeResultString(trainM, trainStd)
+    res['test_time (sec)'] = makeResultString(testM, testStd)
+    res['Accuracy'] = makeResultString(accM, accStd)
+
+    return res
+
+
+def fun_build(name, args):
+    res_fun = name(**args)
+    return res_fun
+
+def param_permutes(model_dict):
+    params = []
+    just_params = removeKey(d, 'model')
+    keys  = just_params.keys()
+    all_together = [just_params[x] for x in keys]
+    param_cross = list(itertools.product(*l))
+    cross_length = len(param_cross)
+    params = [0] * cross_length
+    for i in range(0, cross_length):
+        params[i] = dict(zip(keys, combos[i]))
+    return params
+
+#Borrowed from Rayid
+def precision_at_k(y_true, y_scores, k):
+    threshold = np.sort(y_scores)[::-1][int(k*len(y_scores))]
+    y_pred = np.asarray([1 if i >= threshold else 0 for i in y_scores])
+    return metrics.precision_score(y_true, y_pred)
+
+
 def clf_predict(clf, test_data, feat_names):
     test_feats = test_data[feat_names]
     
