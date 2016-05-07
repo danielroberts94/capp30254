cfc883f2206a7f64ddfd9b05fc63a4f31947bc36
dtroberts94 <danielroberts@uchicago.edu>
2016-05-07 17:34:51 -0500

    update

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,13 +1,13 @@
 #Daniel Roberts Assignment 2
 
-import pipeline as pp
+import newpipeline as pp
 import numpy as np
 
-test_data = pp.data_read('data_input/cs-test.csv')
+#test_data = pp.data_read('data_input/cs-test.csv')
 train_data = pp.data_read('data_input/cs-training.csv')
 
-pp.data_explore(test_data, "test_set")
+#pp.data_explore(test_data, "test_set")
 pp.data_explore(train_data, "train_set")
-
-pp.kfolds_train_only_pipe(test_data, 'SeriousDlqin2yrs', 5)
+results = pp.kfolds_train_only_pipe(train_data, 'SeriousDlqin2yrs', 5)
+pp.write_output("/data_output/all_models_output.csv", results)
 # Attempt Three different classifiers, find accuracies and predicted values
--- assignment3/newpipeline.py
+++ assignment3/newpipeline.py
@@ -1,22 +1,22 @@
 # By Daniel Roberts
+# Significant Parts of the Code below are structually borrowed from some of my Peers
+# In particular, Michael Fosco. Structually, I found his code useful in organizing 
+# my own thoughts about how to loop through the different parameters/models
+# And some of the specific fixes Michael had proved invaluable to me where I would have been stuck 
+# otherwise. As much as possible, I tried to tweak, and rewrite the code.
 
-
+#I also borrow occasionally from Rayid Ghani's "Magic Loop" on github.
+import math
 import numpy as np 
 import pandas as pd 
 from scipy import optimize
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
-from patsy import dmatrices
-import multiprocessing as mp
-from multiprocessing import Process, Queue
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 from sklearn.cross_validation import cross_val_score
 import os, timeit, sys, itertools, re, time, requests, random, functools, logging, csv, datetime
-import seaborn as sns
 from sklearn import linear_model, neighbors, ensemble, svm, preprocessing
-from numba.decorators import jit, autojit
-from numba import double #(nopython = True, cache = True, nogil = True [to run concurrently])
 from sklearn import preprocessing, cross_validation, svm, metrics, tree, decomposition, svm
 from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier, OrthogonalMatchingPursuit, RandomizedLogisticRegression
@@ -32,36 +32,41 @@ from time import time
 '''
 List of models and parameters
 '''
+''
+criteriaHeader = ['AUC', 'Accuracy', 'Function called', 'Precision at .05',
+                  'Precision at .10', 'Precision at .2', 'Precision at .25', 'Precision at .5',
+                  'Precision at .75','Precision at .85','Recall at .05','Recall at .10',
+                  'Recall at .20','Recall at .25','Recall at .5','Recall at .75',
+                  'Recall at .85','f1 at 0.05','f1 at 0.1','f1 at 0.2','f1 at 0.25',
+                  'f1 at 0.5','f1 at 0.75','f1 at 0.85','test_time (sec)','train_time (sec)']
 
-cores = mp.cpu_count()-1
-modelLR = {'model': LogisticRegression, 'solver': ['liblinear'], 'C' : [.01, .1, .5, 1, 5, 10, 25],
-          'class_weight': ['balanced', None], 'n_jobs' : [cores],
-          'tol' : [1e-7, 1e-5, 1e-4, 1e-3, 1e-1, 1], 'penalty': ['l1', 'l2']}
-modelLSVC = {'model': svm.LinearSVC, 'tol' : [1e-7, 1e-5, 1e-4, 1e-3, 1e-1, 1], 'class_weight': ['balanced', None],
-             'max_iter': [1000, 2000], 'C' :[.01, .1, .5, 1, 5, 10, 25]}
-modelKNN = {'model': neighbors.KNeighborsClassifier, 'weights': ['uniform', 'distance'], 'n_neighbors' : [2, 5, 10, 50, 100, 500, 1000, 10000],
-            'leaf_size': [15, 30, 60, 120], 'n_jobs': [cores]}
-modelRF  = {'model': RandomForestClassifier, 'n_estimators': [5, 10, 25, 50, 100, 200, 1000, 10000], 'criterion': ['gini', 'entropy'],
-            'max_features': ['sqrt', 'log2'], 'max_depth': [1, 5, 10, 20, 50, 100], 'min_samples_split': [2, 5, 10, 20, 50],
-            'bootstrap': [True, False], 'n_jobs':[cores]}
-modelET  = {'model': ExtraTreesClassifier, 'n_estimatores': [5, 10, 25, 50, 100, 200, 1000, 10000], 'criterion': ['gini', 'entropy'],
-            'max_features': ['sqrt', 'log2'], 'max_depth': [1, 5, 10, 20, 50, 100],
-            'bootstrap': [True, False], 'n_jobs':[cores]}
-#base classifier for adaboost is automatically a decision tree
-modelAB  = {'model': AdaBoostClassifier, 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1, 10, 100, 200, 1000, 10000]}
-modelSVM = {'model': svm.SVC, 'C':[0.00001,0.0001,0.001,0.01,0.1,1,10], 'probability': [True], 'kernel': ['rbf', 'poly', 'sigmoid']}
-modelGB  = {'model': GradientBoostingClassifier, 'learning_rate': [0.001,0.01,0.05,0.1,0.5], 'n_estimators': [1,10,100], #200,1000,10000], these other numbers took way too long to calc
-            'max_depth': [1,3,5,10,20,50,100], 'subsample' : [0.1, .2, 0.5, 1.0]}
-#Naive Bayes below
+modelNames = ['LogisticRegression', 'KNeighborsClassifier', 'RandomForestClassifier', 'ExtraTreesClassifier',
+              'AdaBoostClassifier', 'SVC', 'GradientBoostingClassifier', 'GaussianNB', 'DecisionTreeClassifier',
+              'SGDClassifier']
+depth = [10, 20, 50]
+modelLR = {'model': LogisticRegression, 'solver': ['sag'], 'C' : [.01, .1, .5, 1],
+          'class_weight': ['balanced', None],'tol' : [1e-5, 1e-3, 1], 'penalty': ['l2']} 
+modelKNN = {'model': neighbors.KNeighborsClassifier, 'weights': ['uniform', 'distance'], 'n_neighbors' : [100, 500, 1000],
+            'leaf_size': [60, 120],} 
+modelRF  = {'model': RandomForestClassifier, 'n_estimators': [25, 50, 100], 'criterion': ['gini', 'entropy'],
+            'max_features': ['sqrt', 'log2'], 'max_depth': depth, 'min_samples_split': [20, 50], 
+            'bootstrap': [True],} 
+modelET  = {'model': ExtraTreesClassifier, 'n_estimators': [25, 50, 100], 'criterion': ['gini', 'entropy'],
+            'max_features': ['sqrt', 'log2'], 'max_depth': depth,
+            'bootstrap': [True, False]}
+modelAB  = {'model': AdaBoostClassifier, 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [5, 10, 25, 50, 100]}#, 200]}
+
+modelSVM = {'model': svm.SVC, 'C':[0.1,1], 'max_iter': [1000, 2000], 'probability': [True], 
+            'kernel': ['rbf', 'poly', 'sigmoid', 'linear']}
+
 modelNB  = {'model': GaussianNB}
-modelDT  = {'model': DecisionTreeClassifier, 'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 
-            'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10, 20, 50]}
-modelSGD = {'model': SGDClassifier, 'loss': ['modified_huber', 'perceptron'], 'penalty': ['l1', 'l2', 'elasticnet'], 
-            'n_jobs': [cores]}
+modelDT  = {'model': DecisionTreeClassifier, 'criterion': ['gini', 'entropy'], 'max_depth': [10,20,50],
+            'max_features': ['sqrt','log2'],'min_samples_split': [10, 20, 50]} 
+modelSGD = {'model': SGDClassifier, 'loss': ['modified_huber', 'perceptron'], 'penalty': ['l1', 'l2', 'elasticnet'], }
 
-modelList = [modelLR, modelLSVC, modelKNN, modelRF, modelET, 
-             modelAB, modelSVM, modelGB, modelNB, modelDT,
-             modelSGD]
+modelList = [modelLR, modelKNN, modelRF, modelET, 
+             modelAB, modelSVM, modelNB, modelDT,
+             modelSGD] 
 
 ##################################################################################
 
@@ -83,29 +88,34 @@ def data_explore(data, dataset_name):
     modes = data.mode(axis=0)
     print modes
     num_data = data.select_dtypes(include = ['number'])
-    if num_data.empty == False
+    if num_data.empty == False:
         for col in num_data:
-            if col != ''
             plot_hist(data[col], str(col), 20, dataset_name, 'hist')
             max_val = data[col].max()
-            tenth_val = data[col].quan
             mean_val = data[col].mean()
             min_val = data[col].min()
             sd_val = data[col].std()
-            if abs(max_val-mean_val)/(sd_val) || abs(max_val-mean_val)/(sd_val)  > 8:
-                log_vals = data[col].apply(log)
-                plot_hist(log_vals, "log" + "_" str(col), 20, dataset_name, 'hist')
+            if abs(max_val-mean_val)/(sd_val) >8 or abs(max_val-mean_val)/(sd_val) > 8:
+                data[col] = data[col][data[col] > 0]
+                log_vals = data[col].apply(np.log)
+                plot_hist(log_vals, "log" + "_" + str(col), 20, dataset_name, 'hist')
 
     str_data = data.select_dtypes(include = ['object'])
-    if str_data.empty == False
+    if str_data.empty == False:
         for col in str_data:
             plot_hist(data[col], str(col), 0, dataset_name, 'bar')
 
 def plot_hist(df, title, num_bins, dataset_name, kind):
         plt.figure()
         if kind == 'hist':
-            binwidth = floor(df.max()- df.min())/num_bins
-            df.hist(bins=np.arange(min(df.min()), max(f.max()) + binwidth, binwidth))
+            binwidth = ((df.max()- df.min())/num_bins)
+            if binwidth > 1:
+                binwidth = math.floor(binwidth)
+            try:
+                df.hist(bins=np.arange(df.min(), df.max() + binwidth, binwidth))
+            except:
+                plt.close()
+                return
         elif kind == 'bar':
             df.value_counts().plot(kind='bar')
         plt.title(title)
@@ -137,58 +147,85 @@ def data_dummify(data, col_names, quantiles):
     return data
 
 #Builds one of three types of classifiers at present. Future versions will be amenable to parameter choice
-def build_classifier(X_var, y_var, k, model_dict):
+def build_classifier(x_var, y_var, k, model_dict):
     params = param_permutes(model_dict)
     model_num = len(params)
-    results = [None]*total
+    print model_num
+    results = [None]*model_num
     folds = cross_validation.KFold(len(y_var), k)
-
     j = 0
     for pars in params:
-        clf = fun_build(model_dict['model'], pars)
-        i = 0
+        if j > 0:
+            break
+        clf = model_dict['model'](**pars)
         train_times = [None]*k
         pred_probs = [None]*k
         test_times = [None]*k
         y_tests = [None]*k
         accs = [None]*k
-
-        for train, test in kf:
-            x_train, x_test = X._slice(train, 0), X._slice(test, 0)
-            y_train, y_test = y._slice(train, 0), y._slice(test, 0)
-            y_tests[i] - yTest
-
-            train_start = time()
-            model_fit = clf.fit(x_train,y_train)
-            train_time = time() - train_start
-            train_times[i] = train_time
-            test_start = time()
-            pred_prob = model_fit.predict_proba(x_test)[:,1]
-            test_time = time() - test_start
-            test_times[i] = test_time
-            pred_probs[i] = pred_prob
-            accs[i] = model_fit.score(XTest,yTest)
-            i += 1
+        #try:
+        i = 0
+        try:
+            for train, test in folds:
+                #if i > 1:
+                #    break
+                print str(i) + "beginning the olfy loop!"
+                x_train, x_test = x_var._slice(train, 0), x_var._slice(test, 0)
+                y_train, y_test = y_var._slice(train, 0), y_var._slice(test, 0)
+                y_tests[i] = y_test
+                train_start = time()
+                model_fit = clf.fit(x_train, y_train)
+                time()
+                train_time = time() - train_start
+                train_times[i] = train_time
+                test_start = time()
+                pred_prob = model_fit.predict_proba(x_test)[:,1]
+                test_time = time() - test_start
+                test_times[i] = test_time
+                pred_probs[i] = pred_prob
+                accs[i] = model_fit.score(x_test,y_test)
+                i += 1 
+                print i
+        except:
+            print "Parameter Values Invalid"
+            continue
+        #if i > 1:
+        #    break
         evals = evaluation(y_tests, pred_probs, train_times, test_times, accs, str(clf))
         results[j] = evals
+        print "after " + str(j)
         j += 1
+    # THIS IS A CRITERIA THAT CAN BE CHANGED DEPENDING ON THE DESIRED RESULTS
+    # IN THIS CASE I USE AUC.
+    # Joint Maximum, Joint Minima, and a variety of other combinations of features can be used to subset here to find
+    # the best parameter set in a given model. This function call can be ommitted to output all possible parameter sets.
+    print results
+    results = maxim_param('AUC', results)
     return results
 
+def maxim_param(param_name, results):
+    param = [mod[param_name] for mod in results if mod != None]
+    max_par = max(param)
+    no_nones =  [x for x in results if x != None]
+    new_res =  [x for x in no_nones if x[param_name] == max_par]
+    return new_res
 
 def evaluation(y_test, pred_probs, train_times, test_times, accuracies, model_name):
-    prec_giv_threshhold = ['Precision at .05', 'Precision at .10', 'Precision at .2', 'Precision at .25', 'Precision at .5', 'Precision at .75', 'Precision at .85']
-    recall_giv_threshhold = ['Recall at .05', 'Recall at .10', 'Recall at .20', 'Recall at .25', 'Recall at .5', 'Recall at .75', 'Recall at .85']
+    levels = ['Precision at .05', 'Precision at .10', 'Precision at .2', 'Precision at .25', 'Precision at .5', 'Precision at .75', 'Precision at .85']
+    recalls = ['Recall at .05', 'Recall at .10', 'Recall at .20', 'Recall at .25', 'Recall at .5', 'Recall at .75', 'Recall at .85']
     levels= [.05, .1, .2, .25, .5, .75, .85]
     levnums = len(levels)
     results = {}
     num_folds = len(y_test)
     folds = range(0, num_folds)
     results['Model'] = model_name
-    for x in xrange(0, tots):
-        threshhold = levels[x]
-        y_pred = np.asarray([1 if i >= threshold else 0 for i in pred_probs])
-        prec = [metrics.precision_score(y_test[j], y_pred) for j in folds]
-        rec = [metrics.recall_score(y_test[j], y_pred) for j in folds]
+    for x in xrange(0, levnums):
+        threshold = levels[x]
+        y_pred = [0]*num_folds
+        for j in folds:
+            y_pred[j] = np.asarray([1 if i >= threshold else 0 for i in pred_probs[j]])
+        prec = [metrics.precision_score(y_test[j], y_pred[j]) for j in folds]
+        rec = [metrics.recall_score(y_test[j], y_pred[j]) for j in folds]
         precStd = np.std(prec)
         recStd = np.std(rec)
 
@@ -198,11 +235,11 @@ def evaluation(y_test, pred_probs, train_times, test_times, accuracies, model_na
         precM = np.mean(prec)
         recM = np.mean(rec)
         f1M = np.mean(f1S)
-        results[levels[x]] = makeResultString(precM, precStd)
-        results[recalls[x]] = makeResultString(recM, recStd)
-        results['f1 at ' + str(thresh)] = makeResultString(f1M, f1Std)
+        results[levels[x]] =  str(precM) + ' (' + str(precStd) + ')'
+        results[recalls[x]] =  str(recM) + ' (' + str(recStd) + ')'
+        results['f1 at ' + str(threshold)] =  str(f1M) + ' (' + str(f1Std) + ')'
 
-    auc = [metrics.roc_auc_score(y_test[j], predProbs[j]) for j in folds]
+    auc = [metrics.roc_auc_score(y_test[j], pred_probs[j]) for j in folds]
     aucStd = np.std(auc)
     aucM = np.mean(auc)
     trainM = np.mean(train_times)
@@ -212,96 +249,47 @@ def evaluation(y_test, pred_probs, train_times, test_times, accuracies, model_na
     accM = np.mean(accuracies)
     accStd = np.std(accuracies)
 
-    results['AUC'] = makeResultString(aucM, aucStd)
-    results['train_time (sec)'] = makeResultString(trainM, trainStd)
-    results['test_time (sec)'] = makeResultString(testM, testStd)
-    results['Accuracy'] = makeResultString(accM, accStd)
+    results['AUC'] = str(aucM) + ' (' + str(aucStd) + ')'
+    results['train_time (sec)'] = str(trainM) + ' (' + str(trainStd) + ')'
+    results['test_time (sec)'] = str(testM) + ' (' + str(testStd) + ')'
+    results['Accuracy'] = str(accM) + ' (' + str(accStd) + ')'
 
     return results
 
-def makeResultString(mean, std):
-    return str(mean) + ' (' + str(std) + ')' 
-
-
-def fun_build(name, args):
-    res_fun = name(**args)
-    return res_fun
+def removeKey(d, key):
+    r = dict(d)
+    del r[key]
+    return r
 
 def param_permutes(model_dict):
     params = []
-    just_params = removeKey(d, 'model')
+    just_params = removeKey(model_dict, 'model')
     keys  = just_params.keys()
     all_together = [just_params[x] for x in keys]
-    param_cross = list(itertools.product(*l))
+    param_cross = list(itertools.product(*all_together))
     cross_length = len(param_cross)
     params = [0] * cross_length
     for i in range(0, cross_length):
-        params[i] = dict(zip(keys, combos[i]))
+        params[i] = dict(zip(keys, param_cross[i]))
     return params
 
-#Borrowed from Rayid
-def precision_at_thresh(y_true, y_scores, thresh):
-    y_pred = np.asarray([1 if i >= k else 0 for i in y_scores])
-    return metrics.precision_score(y_true, y_pred)
-
-def precision_at_k(y_true, y_scores, k):
-    y_pred = np.asarray([1 if i >= k else 0 for i in y_scores])
-    return metrics.precision_score(y_true, y_pred)
-
-def clf_predict(clf, test_data, feat_names):
-    test_feats = test_data[feat_names]
-    
-    pred_values = clf.predict(test_feats)
-    return pred_values
-
-#Determine accuracy from a given classifier
-def clf_accuracy(clf, predictors, predicted):
-    return clf.score(predictors, predicted)
-
-def kfolds_train_only_pipe(data, models = modelList, y_name, folds, fill = mean_mode_fill):
+def kfolds_train_only_pipe(data, y_name, folds, models = [modelRF], fill = mean_mode_fill):
     data = mean_mode_fill(data)
-    y_var = data[yName]
-    x_var = data.drop(yName, 1)
+    y_var = data[y_name]
+    x_var = data.drop(y_name, 1)
     results = []
     for mod in models:
-        mod_result = build_classifier(x_var, y_var, k, mod)
+        mod_result = build_classifier(x_var, y_var, folds, mod)
         results += mod_result
     return results
 
-def plot_precision_recall_n(y_true, y_prob, model_name):
-    from sklearn.metrics import precision_recall_curve
-    y_score = y_prob
-    precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_score)
-    precision_curve = precision_curve[:-1]
-    recall_curve = recall_curve[:-1]
-    pct_above_per_thresh = []
-    number_scored = len(y_score)
-    for value in pr_thresholds:
-        num_above_thresh = len(y_score[y_score>=value])
-        pct_above_thresh = num_above_thresh / float(number_scored)
-        pct_above_per_thresh.append(pct_above_thresh)
-    pct_above_per_thresh = np.array(pct_above_per_thresh)
-    plt.clf()
-    fig, ax1 = plt.subplots()
-    ax1.plot(pct_above_per_thresh, precision_curve, 'b')
-    ax1.set_xlabel('percent of population')
-    ax1.set_ylabel('precision', color='b')
-    ax2 = ax1.twinx()
-    ax2.plot(pct_above_per_thresh, recall_curve, 'r')
-    ax2.set_ylabel('recall', color='r')
-    
-    name = model_name
-    plt.title(name)
-    #plt.savefig(name)
-    plt.show()
-
-def makeHeader(d):
+def make_header(output_dic):
     header = None
     spot = 0
-    limit = len(d)
+    limit = len(output_dic)
     while(header == None and spot < limit):
-        if d[spot] != None:
-            header = [x for x in d[spot].keys()]
+        if output_dic[spot] != None:
+            header = [x for x in output_dic[spot].keys()]
 
     if header == None:
         raise Exception('Unable to grab appropriate header. Please check pipeLine')
@@ -309,17 +297,29 @@ def makeHeader(d):
     header.sort()
     return header
 
-def writeResultsToFile(fName, d):
-    header = makeHeader(d)
-    fin = formatData(header, d)
-    fin.insert(0, header)
-    try:
-        with open(fName, "w") as fout:
-            writer = csv.writer(fout)
-            for f in fin:
-                writer.writerow(f)
-            fout.close()
-    except:
-        return -1
-    return 0
+def format_data(header, output_dic):
+    length = len(output_dic)
+    format = [[]] * length
+    lenMH = len(header)
 
+    outdex = 0
+    index = 0
+    for x in output_dic:
+        temp = [None] * lenMH
+        for j in header:
+            temp[index] = x[j]
+            index += 1
+        index = 0
+        format[indxForm] = temp
+        outdex += 1
+    return format
+
+def write_output(file_name, output_dic):
+    header = make_header(output_dic)
+    fin = format_data(header, output_dic)
+    fin.insert(0, header)
+    with open(file_name, "w") as fout:
+        writer = csv.writer(fout)
+        for f in fin:
+            writer.writerow(f)
+        fout.close()
