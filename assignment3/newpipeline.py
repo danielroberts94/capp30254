426495aadad5447d90a02554b5df320429674095
Daniel Roberts <danielroberts@hitmonchan.cs.uchicago.edu>
2016-05-06 21:57:02 -0500

    new

--- assignment3/newpipeline.py
+++ assignment3/newpipeline.py
@@ -83,30 +83,37 @@ def data_explore(data, dataset_name):
     modes = data.mode(axis=0)
     print modes
     num_data = data.select_dtypes(include = ['number'])
-    for col in num_data:
-        plt.figure()
-        try:
-            data[col].hist(bins=100)
-            plt.title(col)
-            file_path = 'graphics/' + dataset_name + "_" + str(col) + '.png'
-            plt.savefig(file_path)
-            plt.close()
-        except:
-            plt.close()
-            continue
+    if num_data.empty == False
+        for col in num_data:
+            if col != ''
+            plot_hist(data[col], str(col), 20, dataset_name, 'hist')
+            max_val = data[col].max()
+            tenth_val = data[col].quan
+            mean_val = data[col].mean()
+            min_val = data[col].min()
+            sd_val = data[col].std()
+            if abs(max_val-mean_val)/(sd_val) || abs(max_val-mean_val)/(sd_val)  > 8:
+                log_vals = data[col].apply(log)
+                plot_hist(log_vals, "log" + "_" str(col), 20, dataset_name, 'hist')
+
     str_data = data.select_dtypes(include = ['object'])
-    for col in str_data:
+    if str_data.empty == False
+        for col in str_data:
+            plot_hist(data[col], str(col), 0, dataset_name, 'bar')
+
+def plot_hist(df, title, num_bins, dataset_name, kind):
         plt.figure()
-        try:
-            data[col].value_counts().plot(kind='bar')
-            plt.title(col)
-            file_path = 'graphics/' + dataset_name + "_" + str(col) + '.png'
-            plt.savefig(file_path)
-            plt.close()
-        except:
-            plt.close()
-            continue
-#Simple function to fill variable missing vals with mean or mode of the variables
+        if kind == 'hist':
+            binwidth = floor(df.max()- df.min())/num_bins
+            df.hist(bins=np.arange(min(df.min()), max(f.max()) + binwidth, binwidth))
+        elif kind == 'bar':
+            df.value_counts().plot(kind='bar')
+        plt.title(title)
+        file_path = 'graphics/' + dataset_name + "_" + title + '.png'
+        plt.savefig(file_path)
+        plt.close()
+
+#Simple function to fill variable missing vals with mean or mode of the def
 def mean_mode_fill(data):
     num_data = data.select_dtypes(include = ['number'])
     str_data = data.select_dtypes(include = ['object'])
@@ -130,42 +137,45 @@ def data_dummify(data, col_names, quantiles):
     return data
 
 #Builds one of three types of classifiers at present. Future versions will be amenable to parameter choice
-def build_classifier(X_var, y_var, model_dict):
-    params = makeDicts(model_dict)
+def build_classifier(X_var, y_var, k, model_dict):
+    params = param_permutes(model_dict)
     model_num = len(params)
     results = [None]*total
     folds = cross_validation.KFold(len(y_var), k)
 
-    z = 0
+    j = 0
     for pars in params:
         clf = fun_build(model_dict['model'], pars)
         i = 0
+        train_times = [None]*k
+        pred_probs = [None]*k
+        test_times = [None]*k
+        y_tests = [None]*k
+        accs = [None]*k
+
         for train, test in kf:
             x_train, x_test = X._slice(train, 0), X._slice(test, 0)
             y_train, y_test = y._slice(train, 0), y._slice(test, 0)
-            yTests[i] - yTest
+            y_tests[i] - yTest
 
             train_start = time()
             model_fit = clf.fit(x_train,y_train)
             train_time = time() - train_start
             train_times[i] = train_time
             test_start = time()
+            pred_prob = model_fit.predict_proba(x_test)[:,1]
+            test_time = time() - test_start
+            test_times[i] = test_time
+            pred_probs[i] = pred_prob
+            accs[i] = model_fit.score(XTest,yTest)
+            i += 1
+        evals = evaluation(y_tests, pred_probs, train_times, test_times, accs, str(clf))
+        results[j] = evals
+        j += 1
+    return results
 
 
-
-            
-    clfs = {'log': LogisticRegression,
-            'Kneighbors': KNeighborsClassifier,
-            'LSVC': LinearSVC}
-    if len(train_feats.shape) < 2:
-        train_feats.reshape(len(train_feats),1)
-    if len(train_learnvar.shape) < 2:
-        train_feats.reshape(len(train_learnvar),1)
-    clf = clfs[clf_name]()
-    clf.fit(train_feats,train_learnvar)
-    return clf
-#Predict values based on a given classifer (note: not classifier name)
-def evaluation(y_test, predProbs, train_times, test_times, accuracies, model_name):
+def evaluation(y_test, pred_probs, train_times, test_times, accuracies, model_name):
     prec_giv_threshhold = ['Precision at .05', 'Precision at .10', 'Precision at .2', 'Precision at .25', 'Precision at .5', 'Precision at .75', 'Precision at .85']
     recall_giv_threshhold = ['Recall at .05', 'Recall at .10', 'Recall at .20', 'Recall at .25', 'Recall at .5', 'Recall at .75', 'Recall at .85']
     levels= [.05, .1, .2, .25, .5, .75, .85]
@@ -175,9 +185,24 @@ def evaluation(y_test, predProbs, train_times, test_times, accuracies, model_nam
     folds = range(0, num_folds)
     results['Model'] = model_name
     for x in xrange(0, tots):
-        threshhold = amts[x]
-        prec = [precision_at_k(y_test[j], predProbs[j], threshhold)  for j in folds]
-    auc = [metrics.roc_auc_score(yTests[j], predProbs[j]) for j in folds]
+        threshhold = levels[x]
+        y_pred = np.asarray([1 if i >= threshold else 0 for i in pred_probs])
+        prec = [metrics.precision_score(y_test[j], y_pred) for j in folds]
+        rec = [metrics.recall_score(y_test[j], y_pred) for j in folds]
+        precStd = np.std(prec)
+        recStd = np.std(rec)
+
+        f1S = [2*(prec[j]*rec[j])/(prec[j]+rec[j]) for j in folds]
+        f1Std = np.std(f1S)
+
+        precM = np.mean(prec)
+        recM = np.mean(rec)
+        f1M = np.mean(f1S)
+        results[levels[x]] = makeResultString(precM, precStd)
+        results[recalls[x]] = makeResultString(recM, recStd)
+        results['f1 at ' + str(thresh)] = makeResultString(f1M, f1Std)
+
+    auc = [metrics.roc_auc_score(y_test[j], predProbs[j]) for j in folds]
     aucStd = np.std(auc)
     aucM = np.mean(auc)
     trainM = np.mean(train_times)
@@ -187,12 +212,15 @@ def evaluation(y_test, predProbs, train_times, test_times, accuracies, model_nam
     accM = np.mean(accuracies)
     accStd = np.std(accuracies)
 
-    res['AUC'] = makeResultString(aucM, aucStd)
-    res['train_time (sec)'] = makeResultString(trainM, trainStd)
-    res['test_time (sec)'] = makeResultString(testM, testStd)
-    res['Accuracy'] = makeResultString(accM, accStd)
+    results['AUC'] = makeResultString(aucM, aucStd)
+    results['train_time (sec)'] = makeResultString(trainM, trainStd)
+    results['test_time (sec)'] = makeResultString(testM, testStd)
+    results['Accuracy'] = makeResultString(accM, accStd)
 
-    return res
+    return results
+
+def makeResultString(mean, std):
+    return str(mean) + ' (' + str(std) + ')' 
 
 
 def fun_build(name, args):
@@ -212,11 +240,13 @@ def param_permutes(model_dict):
     return params
 
 #Borrowed from Rayid
-def precision_at_k(y_true, y_scores, k):
-    threshold = np.sort(y_scores)[::-1][int(k*len(y_scores))]
-    y_pred = np.asarray([1 if i >= threshold else 0 for i in y_scores])
+def precision_at_thresh(y_true, y_scores, thresh):
+    y_pred = np.asarray([1 if i >= k else 0 for i in y_scores])
     return metrics.precision_score(y_true, y_pred)
 
+def precision_at_k(y_true, y_scores, k):
+    y_pred = np.asarray([1 if i >= k else 0 for i in y_scores])
+    return metrics.precision_score(y_true, y_pred)
 
 def clf_predict(clf, test_data, feat_names):
     test_feats = test_data[feat_names]
@@ -227,3 +257,69 @@ def clf_predict(clf, test_data, feat_names):
 #Determine accuracy from a given classifier
 def clf_accuracy(clf, predictors, predicted):
     return clf.score(predictors, predicted)
+
+def kfolds_train_only_pipe(data, models = modelList, y_name, folds, fill = mean_mode_fill):
+    data = mean_mode_fill(data)
+    y_var = data[yName]
+    x_var = data.drop(yName, 1)
+    results = []
+    for mod in models:
+        mod_result = build_classifier(x_var, y_var, k, mod)
+        results += mod_result
+    return results
+
+def plot_precision_recall_n(y_true, y_prob, model_name):
+    from sklearn.metrics import precision_recall_curve
+    y_score = y_prob
+    precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_score)
+    precision_curve = precision_curve[:-1]
+    recall_curve = recall_curve[:-1]
+    pct_above_per_thresh = []
+    number_scored = len(y_score)
+    for value in pr_thresholds:
+        num_above_thresh = len(y_score[y_score>=value])
+        pct_above_thresh = num_above_thresh / float(number_scored)
+        pct_above_per_thresh.append(pct_above_thresh)
+    pct_above_per_thresh = np.array(pct_above_per_thresh)
+    plt.clf()
+    fig, ax1 = plt.subplots()
+    ax1.plot(pct_above_per_thresh, precision_curve, 'b')
+    ax1.set_xlabel('percent of population')
+    ax1.set_ylabel('precision', color='b')
+    ax2 = ax1.twinx()
+    ax2.plot(pct_above_per_thresh, recall_curve, 'r')
+    ax2.set_ylabel('recall', color='r')
+    
+    name = model_name
+    plt.title(name)
+    #plt.savefig(name)
+    plt.show()
+
+def makeHeader(d):
+    header = None
+    spot = 0
+    limit = len(d)
+    while(header == None and spot < limit):
+        if d[spot] != None:
+            header = [x for x in d[spot].keys()]
+
+    if header == None:
+        raise Exception('Unable to grab appropriate header. Please check pipeLine')
+
+    header.sort()
+    return header
+
+def writeResultsToFile(fName, d):
+    header = makeHeader(d)
+    fin = formatData(header, d)
+    fin.insert(0, header)
+    try:
+        with open(fName, "w") as fout:
+            writer = csv.writer(fout)
+            for f in fin:
+                writer.writerow(f)
+            fout.close()
+    except:
+        return -1
+    return 0
+
